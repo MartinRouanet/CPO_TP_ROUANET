a9214e11eee999bc7e8249d0c384afaf3eb9a136
Martin  Rouanet <martin.rouanet@epfedu.fr>
2024-09-17 16:59:51 +0200

    Creation du projet exo 1. Affichage de message.

---
+++ TP0_Exo1/src/tp0_exo1/TP0_Exo1.java
@@ -0,0 +1,28 @@
+/*
+ * TP0 Exercice 1
+ * TDC - Groupe 1
+ * Martin Rouanet
+ * 17 Septembre 2024
+ */
+package tp0_exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author marti
+ */
+public class TP0_Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+        Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Salut, quelle jour est-il ?");
+sc.nextLine();
+    }
+    
+}
