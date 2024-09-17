d708770a5fbf8c1ae0fbe21e3aa6bec8036707fe
Martin  Rouanet <martin.rouanet@epfedu.fr>
2024-09-17 17:02:14 +0200

    Création du projet exo 2. Calcul de la somme des n entiers.

---
+++ TP0_Exo2/src/tpo_exo2/TPO_EXO2.java
@@ -0,0 +1,43 @@
+/*
+ * TP0 Exercice 1
+ * TDC - Groupe 1
+ * Martin Rouanet
+ * 17 Septembre 2024
+ */
+package tpo_exo2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author marti
+ */
+public class TPO_EXO2 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//Declaration des variables
+int nb; // nombre dentiers a additionner
+int result; // resultat
+int ind; //indice
+//nb=5;
+result=0;
+Scanner sc = new Scanner(System.in);
+System.out.println("\n Entrer le nombre :");
+nb=sc.nextInt(); // On demande a sc de donner le prochain entier
+// Addition des nb premiers entiers
+ind=1;
+while (ind <= nb) {
+result=result+ind;
+ind++;
+}
+// Affichage du resultat
+System.out.println();
+System.out.println("La somme des "+ nb + "entiers est: "+result);
+        
+    }
+    
+}
